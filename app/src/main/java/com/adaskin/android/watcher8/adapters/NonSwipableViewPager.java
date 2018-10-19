57c03d4e77865a05e3b20817ef473d7f377fc864
dadaskin <dave@adaskin.com>
2018-10-19 15:53:33 -0700

    Initial commit/push for Watcher8

---
+++ app/src/main/java/com/adaskin/android/watcher8/adapters/NonSwipableViewPager.java
@@ -0,0 +1,42 @@
+package com.adaskin.android.watcher8.adapters;
+
+import android.content.Context;
+//import android.support.multidex.MultiDex;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NonSwipableViewPager extends ViewPager {
+
+    public NonSwipableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NonSwipableViewPager(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        // Never allow swiping to switch between pages
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent arg0) {
+        // Never allow swiping to switch between pages
+        performClick();
+        return false;
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+//    @Override
+//    protected void attachBaseContext(Context base) {
+//       // super.attachBaseContext(base);
+//        MultiDex.install(base);
+//    }
+}
